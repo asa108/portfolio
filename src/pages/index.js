c3b26da380f4b4c5b7e802df016ab579fdb418e1
Asane <yaradayo317@gmail.com>
2022-02-12 11:08:50 -0500

    added OG image

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import App from '../components/App';
 import { headData } from '../mock/data';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.scss';
+import socialBanner from '../images/barsmile.jpg';
 
 export default () => {
   const { title, lang, description } = headData;
@@ -15,6 +16,7 @@ export default () => {
         <title>{title || "Asane's portfolio"}</title>
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Welcome to my site!'} />
+        <meta property="og:image" content={socialBanner} />
       </Helmet>
       <App />
     </>
